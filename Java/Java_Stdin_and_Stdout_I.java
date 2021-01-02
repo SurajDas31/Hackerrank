5e63e6d9f6583efadca1fc024878bb6e54245f79
Suraj Das <54069735+SurajDas31@users.noreply.github.com>
2021-01-02 16:28:00 +0530

    Add files via upload

---
+++ Java/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a1 = scan.nextInt();
+        int a2 = scan.nextInt();
+        int a3 = scan.nextInt();
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
